6b57c7de3288e087ee8e218c1b254df04bb63998
Nicolas Märchy <billedtrain380@gmail.com>
2018-10-05 17:00:50 +0200

    Add page routing security

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,62 +1,58 @@
-import { NbMenuItem } from '@nebular/theme';
+import {NbMenuItem} from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  {
-    title: 'Dashboard',
-    icon: 'nb-home',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
-    title: 'Gruppen',
-    icon: 'fa fa-university',
-    link: '/pages/group/overview',
-  },
-  {
-    title: 'Teilnehmerlisten',
-    icon: 'nb-list',
-    link: '/pages/classes',
-  },
-  {
-    title: 'Leichtathletik',
-    icon: 'nb-keypad',
-    link: '/pages/athletics',
-    children: [
-      {
-        title: 'Starliste',
-        link: '/pages/ui-features/buttons',
-      },
-      {
-        title: 'Wettkampfblätter',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Resultate',
-        link: '/pages/athletics/results',
-      },
-      {
-        title: 'Ranglisten',
-        link: '/pages/ui-features/modals',
-      },
-    ],
-  },
-  {
-    title: 'Verwaltung',
-    icon: 'users-class',
-    link: '/pages/management',
-  },
-  {
-    title: 'Einstellungen',
-    icon: 'nb-compose',
-    children: [
-      {
-        title: 'Benutzer',
-        link: '/pages/forms/inputs',
-      },
-      {
-        title: 'PETs',
-        link: '/pages/forms/layouts',
-      },
-    ],
-  },
+    {
+        title: 'Gruppen',
+        icon: 'fa fa-university',
+        link: '/pages/group/overview',
+        home: true,
+    },
+    {
+        title: 'Teilnehmerlisten',
+        icon: 'nb-list',
+        link: '/pages/classes',
+    },
+    {
+        title: 'Leichtathletik',
+        icon: 'nb-keypad',
+        link: '/pages/athletics',
+        children: [
+            // Will not be available until report generation is refactored.
+            // {
+            //     title: 'Startliste',
+            //     link: '/pages/ui-features/buttons',
+            // },
+            {
+                title: 'Wettkampfblätter',
+                link: '/pages/ui-features/grid',
+            },
+            {
+                title: 'Resultate',
+                link: '/pages/athletics/results',
+            },
+            {
+                title: 'Ranglisten',
+                link: '/pages/ui-features/modals',
+            },
+        ],
+    },
+    {
+        title: 'Verwaltung',
+        icon: 'users-class',
+        link: '/pages/management',
+    },
+    {
+        title: 'Einstellungen',
+        icon: 'nb-compose',
+        children: [
+            {
+                title: 'Benutzer',
+                link: '/pages/forms/inputs',
+            },
+            {
+                title: 'PETs',
+                link: '/pages/forms/layouts',
+            },
+        ],
+    },
 ];
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,61 +2,60 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 
 import {PagesComponent} from './pages.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
 import {NotFoundComponent} from './miscellaneous/not-found/not-found.component';
 import {GroupOverviewComponent} from './group/overview/group-overview.component';
 import {GroupDetailComponent} from './group/detail/group-detail.component';
 import {ManagementComponent} from './management/management.component';
+import {AuthGuard, RoleGuard} from '../app.security';
 
 const routes: Routes = [{
-  path: '',
-  component: PagesComponent,
-  children: [{
-    path: 'dashboard',
-    component: DashboardComponent,
-  }, {
-    path: 'group/overview',
-    component: GroupOverviewComponent,
-  }, {
-    path: 'group/detail/:name',
-    component: GroupDetailComponent,
-  }, {
-    path: 'management',
-    component: ManagementComponent,
-  }, {
-    path: 'athletics',
-    loadChildren: './athletics/athletics-page.module#AthleticsPageModule',
-  }, {
-    path: 'maps',
-    loadChildren: './maps/maps.module#MapsModule',
-  }, {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule',
-  }, {
-    path: 'editors',
-    loadChildren: './editors/editors.module#EditorsModule',
-  }, {
-    path: 'forms',
-    loadChildren: './forms/forms.module#FormsModule',
-  }, {
-    path: 'tables',
-    loadChildren: './tables/tables.module#TablesModule',
-  }, {
-    path: 'miscellaneous',
-    loadChildren: './miscellaneous/miscellaneous.module#MiscellaneousModule',
-  }, {
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }, {
-    path: '**',
-    component: NotFoundComponent,
-  }],
+    component: PagesComponent,
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard, RoleGuard],
+    children: [{
+        path: 'group/overview',
+        component: GroupOverviewComponent,
+    }, {
+        path: 'group/detail/:name',
+        component: GroupDetailComponent,
+    }, {
+        path: 'management',
+        component: ManagementComponent,
+    }, {
+        path: 'athletics',
+        loadChildren: './athletics/athletics-page.module#AthleticsPageModule',
+    }, {
+        path: 'maps',
+        loadChildren: './maps/maps.module#MapsModule',
+    }, {
+        path: 'charts',
+        loadChildren: './charts/charts.module#ChartsModule',
+    }, {
+        path: 'editors',
+        loadChildren: './editors/editors.module#EditorsModule',
+    }, {
+        path: 'forms',
+        loadChildren: './forms/forms.module#FormsModule',
+    }, {
+        path: 'tables',
+        loadChildren: './tables/tables.module#TablesModule',
+    }, {
+        path: 'miscellaneous',
+        loadChildren: './miscellaneous/miscellaneous.module#MiscellaneousModule',
+    }, {
+        path: '',
+        redirectTo: 'group/overview',
+        pathMatch: 'full',
+    }, {
+        path: '**',
+        component: NotFoundComponent,
+    }],
 }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class PagesRoutingModule {
 }
