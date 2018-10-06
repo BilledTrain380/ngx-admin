3f063588883f87c58ef26d276377f4ad75feb19f
Nicolas Märchy <billedtrain380@gmail.com>
2018-10-06 12:29:58 +0200

    Add info text if no groups are found

--- src/app/pages/athletics/results/results.component.ts
+++ src/app/pages/athletics/results/results.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {Competitor, Discipline, Result, TemporaryResult} from '../../../modules/athletics/athletics-models';
+import {Discipline, TemporaryResult} from '../../../modules/athletics/athletics-models';
 import {GROUP_PROVIDER, GroupProvider} from '../../../modules/group/group-providers';
 import {Group} from '../../../modules/group/group-models';
 import {SmartSelectSettings} from '../../../modules/smart-select/smart-select-settings';
@@ -10,78 +10,9 @@ import {
     DisciplineProvider,
 } from '../../../modules/athletics/athletics-providers';
 import {Gender} from '../../../modules/participant/participant-models';
-import {Observable, Subject} from 'rxjs';
 import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import {CompetitorModel} from './result.models';
 
-class ResultModel {
-
-    readonly id: number;
-    readonly distance?: string;
-    readonly discipline: Discipline;
-
-    readonly valueProperty: Observable<number>;
-    value: string;
-    isValueValid: boolean = true;
-
-    points: number;
-    readonly unit: string;
-
-    isCalculating: boolean = false;
-
-    private readonly valueEmitter: Subject<number> = new Subject();
-
-    constructor(
-        result: Result,
-    ) {
-        this.id = result.id;
-        this.discipline = result.discipline;
-        this.distance = result.distance;
-        this.value = String(result.value / result.discipline.unit.factor);
-        this.points = result.points;
-        this.unit = result.discipline.unit.name;
-        this.valueProperty = this.valueEmitter.asObservable();
-    }
-
-    validate(): void {
-
-        // just replace comma with decimal point, so it does not matter whenever its a comma or decimal point
-        this.value = this.value.replace(',', '.');
-
-        if (Number.isNaN(Number(this.value))) {
-            this.isValueValid = false;
-            return;
-        }
-
-        this.isValueValid = true;
-    }
-
-    valueChanged(): void {
-        this.validate();
-        if (this.isValueValid) this.valueEmitter.next(Number(this.value));
-    }
-}
-
-class CompetitorModel {
-
-    readonly startNumber: number;
-    readonly prename: string;
-    readonly surname: string;
-    readonly gender: Gender;
-    readonly result?: ResultModel;
-
-    constructor(
-        competitor: Competitor,
-        currentDiscipline: Discipline,
-    ) {
-        this.startNumber = competitor.startNumber;
-        this.prename = competitor.prename;
-        this.surname = competitor.surname;
-        this.gender = competitor.gender;
-        this.result = new ResultModel(
-            competitor.results.find(it => it.discipline.name === currentDiscipline.name),
-        );
-    }
-}
 
 @Component({
     selector: 'ngx-results',
@@ -157,9 +88,9 @@ export class ResultsComponent implements OnInit {
 
     private async loadCompetitors(): Promise<void> {
 
-        this.isLoading = true;
-
         if (!(this.selectedGroup && this.selectedDiscipline)) return;
+
+        this.isLoading = true;
 
         let gender: Gender|undefined;
 
@@ -209,5 +140,3 @@ export class ResultsComponent implements OnInit {
         this.isLoading = false;
     }
 }
-
-
